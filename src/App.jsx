a707338ea34c20563db3f5f9b6adf3b56e49cf90
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-01-14 19:52:18 +0100

    Añadida la navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,9 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <header>
+        <header className='w-full flex gap-5 py-[24px] px-[40px]'>
             <h1 className='text-3xl text-white'>RAWG</h1>
+            <input type='text' placeholder="Search ..." className='bg-gray-800 text-white rounded-full px-4 py-2'></input>
         </header>
     )
 }
