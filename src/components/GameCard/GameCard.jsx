61a11d851fd2d38ca89076e853fb68973d3051d8
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-01-16 21:49:49 +0100

    Cambios en el diseño de la lista y las tarjetas

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,10 +1,31 @@
 import './GameCard.css';
 
 const GameCard = ({ game }) => {
+    const genres = game.genres;
     return (
-        <div className='text-white'>
-            <img className="h-auto max-w-full rounded-lg" src={game.background_image} alt="" />
-            <span>{game.name}</span>
+        <div className='flex flex-col group gap-5 hover:scale-110 transition-transform text-white bg-[#202020] rounded-md text-left'>
+            <img className="w-full h-[200px] object-cover max-w-full rounded-md"
+                src={game.background_image}
+                alt={game.name} />
+            <div className='p-3 flex flex-col gap-5'>
+                <span className='text-3xl font-semibold text-balance'>{game.name}</span>
+                <div className='flex-col gap-5 hidden group-hover:flex group-hover:relative'>
+                    <div className='flex justify-between'>
+                        <span>Release Date:</span>
+                        <span className=''>{game.released}</span>
+                    </div>
+                    <div className='flex justify-between'>
+                        <span>Genres:</span>
+                        <div className='flex gap-2'>
+                            {
+                                genres.length > 0 && genres.map(genre =>
+                                    <span key={genre.id}>{genre.name}</span>
+                                )
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/GameList/GameList.jsx
+++ src/components/GameList/GameList.jsx
@@ -5,7 +5,7 @@ const GameList = ({ games }) => {
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
             {
-                games?.map(game => <GameCard game={game} />)
+                games?.map(game => <GameCard key={game.id} game={game} />)
             }
         </div>
     )
