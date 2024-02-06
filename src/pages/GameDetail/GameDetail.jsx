52c89dfbffa0a6b6bcbd58cae4ef903a140a760a
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-02-06 21:51:09 +0100

    Cambios en el diseño del detalle del juego

--- src/pages/GameDetail/GameDetail.jsx
+++ src/pages/GameDetail/GameDetail.jsx
@@ -5,7 +5,7 @@ import parse from "html-react-parser";
 import GameSearchService from '../../services/GameSearch.service';
 
 const GameDetail = () => {
-    const [game, setGame] = useState([]);
+    const [game, setGame] = useState(null);
     const { id } = useParams();
 
     useEffect(() => {
@@ -18,17 +18,26 @@ const GameDetail = () => {
     return (
         <>
             {
-                game &&
+                game !== null &&
                 <div className='w-full'>
                     <div className='container-image z-0' style={{
                         background: `linear-gradient(rgba(15, 15, 15, 0), rgb(21, 21, 21)), linear-gradient(rgba(21, 21, 21, 0.8), rgba(21, 21, 21, 0.5)),url(${game.background_image})`
                     }}>
                     </div>
-                    <h1 className='text-white'>
-                        {game.name}
-                    </h1>
-                    <div className='text-white text-left'>
-                        {parse(String(game.description))}
+                    <div className='flex justify-center mt-[32px]'>
+                        <div className='flex max-w-[960px]'>
+                            <div className='text-left'>
+                                <h1 className='text-white text-[72px] leading-[74px] font-[700] mb-[48px]'>
+                                    {game.name}
+                                </h1>
+                                <div className='text-white text-left'>
+                                    {parse(String(game.description))}
+                                </div>
+                            </div>
+                            <div className='max-w-[384px] ml-[48px]'>
+                                <img src={game.background_image} alt={game.name} />
+                            </div>
+                        </div>
                     </div>
                 </div>
             }
