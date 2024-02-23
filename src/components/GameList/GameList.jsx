89c5ec569383d9f43d7588c1972ee92d0ba25444
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-02-23 22:54:05 +0100

    Cambio en el diseño responsivo.

--- src/components/GameList/GameList.jsx
+++ src/components/GameList/GameList.jsx
@@ -3,7 +3,7 @@ import './GameList'
 
 const GameList = ({ games }) => {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
             {
                 games?.length > 0 && games.map((game) => <GameCard key={game.id} game={game} />)
             }
