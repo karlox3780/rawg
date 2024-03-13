97158b08fd17f4271e509c3ed4935e68cd1aa9ba
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-03-13 23:57:05 +0100

    Cambio en el color del texto.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = ({ filterGames }) => {
 
                     <div className='dotted-label'></div>
                 </div>
-                <button onClick={handleShowNavbar} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-[hsla(0,0%,100%,.2)] focus:outline-none focus:ring-2 focus:ring-[hsla(0,0%,100%,.2)] dark:text-[hsla(0,0%,100%,.2)] dark:hover:bg-[hsla(0,0%,100%,.2)] dark:focus:ring-[hsla(0,0%,100%,.2)]" aria-controls="navbar-default" aria-expanded="false">
+                <button onClick={handleShowNavbar} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-[hsla(0,0%,100%,.2)] rounded-lg md:hidden hover:bg-[hsla(0,0%,100%,.2)] focus:outline-none focus:ring-2 focus:ring-[hsla(0,0%,100%,.2)] dark:text-[hsla(0,0%,100%,.2)] dark:hover:bg-[hsla(0,0%,100%,.2)] dark:focus:ring-[hsla(0,0%,100%,.2)]" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
